57e15850d8c9533cdb3eb660c2572e7d17adc297
nocturnalq <leonidfilippov256@gmail.com>
2022-08-11 18:06:13 +0300

    i am tired off parsing html pages

--- app/main.py
+++ app/main.py
@@ -1,4 +1,35 @@
+from cgitb import reset
 import telebot
-from dotenv import load_dotenv
+import dotenv
+import requests
+from bs4 import BeautifulSoup
+from pathlib import Path
+import os
 
-bot = telebot.TeleBot('$TELEGRAM_TOKEN')
+def parse_table(committee_url):
+    url = committee_url
+    html_text = requests.get(url)
+    soup = BeautifulSoup(html_text.content, 'html5lib')
+    table = soup.find("table", class_="namesTable")
+    # print(table)
+    for row in table.find_all('tbody'):
+        fio = row.find_all('td', class_='fio')
+        num = row.find_all('td', class_='num')
+        
+    
+
+def load_envs(path: str) -> str:
+    dotenv_path = Path(path)
+    dotenv.load_dotenv(dotenv_path=dotenv_path)
+
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+def main():
+    #  bot = telebot.TeleBot(TELEGRAM_TOKEN)
+    parse_table('https://priem.mirea.ru/accepted-entrants-list/personal_code_rating.php?competition=1714961437918539062&prior=any&documentType=original&accepted=1&acceptedEntrant=any&onlyActive=1&onlyPaid=0')
+
+
+
+if __name__ == '__main__':
+    main()
