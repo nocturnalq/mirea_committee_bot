b83ef95690c924eccdedbca8c6c7d3e5e1c72cce
nocturnalq <leonidfilippov256@gmail.com>
2022-08-11 19:32:37 +0300

    parser logic done

--- app/main.py
+++ app/main.py
@@ -6,24 +6,32 @@ from bs4 import BeautifulSoup
 from pathlib import Path
 import os
 
-def parse_table(committee_url):
-    url = committee_url
-    html_text = requests.get(url)
-    soup = BeautifulSoup(html_text.content, 'html5lib')
-    table = soup.find("table", class_="namesTable")
-    # print(table)
-    for row in table.find_all('tbody'):
-        fio = row.find_all('td', class_='fio')
-        num = row.find_all('td', class_='num')
-        
-    
-
 def load_envs(path: str) -> str:
     dotenv_path = Path(path)
     dotenv.load_dotenv(dotenv_path=dotenv_path)
 
-
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+SNILS = os.getenv("SNILS")
+
+def parse_table(url):
+    snils_list = SNILS.split(',')
+    html_text = requests.get(url)
+    soup = BeautifulSoup(html_text.content, 'html5lib')
+    table = soup.find("table", class_="namesTable")
+    i = 0
+    pos = {}
+    for row in table.find_all(attrs={'class':'fio'})[1:]:
+        i += 1
+        if row.text in snils_list:
+            # print(i, row.text)
+            pos.update({f"{i}":f"{row.text}"})
+    print(pos)
+    
+
+
+
+
+
 
 def main():
     #  bot = telebot.TeleBot(TELEGRAM_TOKEN)
